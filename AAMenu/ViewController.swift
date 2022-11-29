879a369c5ac88a0e351b0cffd9124ffeeff2c702
kwonOhHyeon <ohyoon0425@fixelsoft.com>
2022-11-30 00:08:12 +0900

    Initial Commit

---
+++ AAMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AAMenu
+//
+//  Created by 권오현 on 2022/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
